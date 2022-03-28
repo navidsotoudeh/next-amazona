26024066299a328eabd5f23fcbf6b3738b299a2a
Navid <navidsotoudehmehr@gmail.com>
2022-03-28 10:15:01 +0430

    shipping page created

--- utils/Store.js
+++ utils/Store.js
@@ -1,26 +1,32 @@
-import Cookies from "js-cookie";
-import { createContext, useReducer } from "react";
+import Cookies from 'js-cookie';
+import { createContext, useReducer } from 'react';
 
 export const Store = createContext();
 const initialState = {
-  darkMode: Cookies.get("darkMode") === "ON" ? true : false,
+  darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
   cart: {
-    cartItems: Cookies.get("cartItems")
-      ? JSON.parse(Cookies.get("cartItems"))
+    cartItems: Cookies.get('cartItems')
+      ? JSON.parse(Cookies.get('cartItems'))
       : [],
+    shippingAddress: Cookies.get('shippingAddress')
+      ? JSON.parse(Cookies.get('shippingAddress'))
+      : { location: {} },
+    paymentMethod: Cookies.get('paymentMethod')
+      ? Cookies.get('paymentMethod')
+      : '',
   },
-  userInfo: Cookies.get("userInfo")
-    ? JSON.parse(Cookies.get("userInfo"))
+  userInfo: Cookies.get('userInfo')
+    ? JSON.parse(Cookies.get('userInfo'))
     : null,
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "DARK_MODE_ON":
+    case 'DARK_MODE_ON':
       return { ...state, darkMode: true };
-    case "DARK_MODE_OFF":
+    case 'DARK_MODE_OFF':
       return { ...state, darkMode: false };
-    case "CART_ADD_ITEM": {
+    case 'CART_ADD_ITEM': {
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
@@ -30,20 +36,57 @@ function reducer(state, action) {
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
-      Cookies.set("cartItems", JSON.stringify(cartItems));
+      Cookies.set('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
-    case "CART_REMOVE_ITEM": {
+    case 'CART_REMOVE_ITEM': {
       const cartItems = state.cart.cartItems.filter(
         (item) => item._id !== action.payload._id
       );
-      Cookies.set("cartItems", JSON.stringify(cartItems));
+      Cookies.set('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
-    case "USER_LOGIN":
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
+        },
+      };
+    case 'SAVE_SHIPPING_ADDRESS_MAP_LOCATION':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            location: action.payload,
+          },
+        },
+      };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
+      };
+    case 'CART_CLEAR':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
+    case 'USER_LOGIN':
       return { ...state, userInfo: action.payload };
-    case "USER_LOGOUT":
-      return { ...state, userInfo: null, cart: { cartItems: [] } };
+    case 'USER_LOGOUT':
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: { location: {} },
+          paymentMethod: '',
+        },
+      };
 
     default:
       return state;
